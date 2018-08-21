7fd2bffc4e1131d8d8bdd2551b2a63cef35bca18
sulikoks <42274724+sulikoks@users.noreply.github.com>
2018-08-21 23:12:35 +0300

    fizzBuzz
    Реализуйте и экспортируйте по умолчанию функцию, которая выводит (console.log) в терминал числа в диапазоне от begin до end. При этом:
    
    Если число делится без остатка на 3, то вместо него выводится слово Fizz
    Если число делится без остатка на 5, то вместо него выводится слово Buzz
    Если число делится без остатка и на 3, и на 5, то вместо числа выводится слово FizzBuzz
    В остальных случаях выводится само число
    Функция принимает два параметра (begin и end), определяющих начало и конец диапазона (включительно). Если диапазон пуст (в случае, когда begin > end), то функция просто ничего не печатает.

---
+++ myProgrammJS/fizzBuzz.js
@@ -0,0 +1,9 @@
+const fizzBuzz = (begin, end) => {
+  for (let i = begin; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz');
+    else if (i % 3 === 0) console.log('Fizz');
+    else if (i % 5 === 0) console.log('Buzz');
+    else console.log(i);
+  }
+}
+export default fizzBuzz;
